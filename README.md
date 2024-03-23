2debb38f3f13fb3bfb4e9b809778921de6cc4b47
CJMAXiK <me@cjmaxik.com>
2024-03-23 14:53:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Idle Fishing for HoloCure
+
+Uses OpenCV, Pillow, and Windows API to automate HoloCure's fishing mini-game.
+
+> [!NOTE]  
+> Please use this script only when you played HoloCure for a good amount of time by yourself. It practically breaks the economy and, as a result, fun.
+>
+> Please support the creator by giving them a review [on Steam](https://store.steampowered.com/app/2420510/HoloCure__Save_the_Fans/).
+
+## How to use
+
+1. Install Python 3.12 or later
+2. `pip install -r requirements.txt` (I recommend creating a `venv` first)
+3. Open HoloCure, make it windowed, navigate to Holo House
+4. `python idle-fishing.py`, Esc menu should pop up
+5. Initiate fishing manually
+6. ...
+7. PROFIT!
+8. Halt the script when you want to stop fishing
+
+The HoloCure window must be on the foreground, but you can still use your PC as usual as the script does not interfere with your inputs.
+
+Make sure your mouse is not on top of the HoloCure window (specifically the target area of the phishing mini-game).
+
+## Known issues
+
+- Working only on Windows
+- Tested only with 1280x720 resolution of the HoloCure window
+- Template matching might fail when the keys are too close to each other and/or on higher speed levels.
---
+++ idle-fishing.py
@@ -0,0 +1,111 @@
+from PIL import ImageGrab
+import cv2
+import numpy as np
+import win32gui, win32api, win32con
+import time
+
+DEBUG = False
+threshold = 0.8
+
+# HoloCure window
+windowName = "HoloCure"
+hwndMain = 0
+
+
+def grab_frame(hwnd):
+    if not win32gui.IsWindow(hwnd):
+        return None
+
+    img = ImageGrab.grab(
+        bbox=win32gui.GetWindowRect(hwnd),
+        include_layered_windows=True,
+        all_screens=True,
+    )
+
+    # debug: show imagegrab result
+    if DEBUG:
+        cv2.imshow("Main Image", np.array(img))
+
+    return img.crop((747, 480, 850, 570))
+
+
+def generate_patterns():
+    patterns = []
+    for i in ["up", "down", "center", "left", "right", "window"]:
+        pattern = cv2.imread("./patterns/%s.png" % i, cv2.COLOR_RGB2GRAY)
+        pattern, *_ = cv2.split(pattern)
+        patterns.append((i, pattern))
+
+    return patterns
+
+
+def match_inputs(pattern):
+    match pattern:
+        case "up":
+            return [0x57]
+        case "down":
+            return [0x53]
+        case "center":
+            return [0x20]
+        case "left":
+            return [0x41]
+        case "right":
+            return [0x44]
+        case "window":
+            return [0x0D, 0.5, 0x0D]
+        case _:
+            return None
+
+
+def main():
+    patterns = generate_patterns()
+
+    hwndMain = win32gui.FindWindow(None, "HoloCure")
+    if not hwndMain:
+        print("HoloCure window not found")
+        return
+    win32gui.SetForegroundWindow(hwndMain)
+
+    # Send escape key as a test
+    win32api.PostMessage(hwndMain, win32con.WM_KEYDOWN, win32con.VK_ESCAPE, 0)
+
+    while True:
+        sc = grab_frame(hwndMain)
+        if sc is None:
+            print("HoloCure window not found")
+            return
+
+        # Convert to grayscale and threshold
+        img = cv2.cvtColor(np.array(sc), cv2.COLOR_RGB2GRAY)
+        _, img = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+
+        # debug: show the resulted image
+        if DEBUG:
+            cv2.imshow("Result", img)
+            cv2.waitKey(1)
+
+        # Perform template matching
+        for pattern in patterns:
+            r = cv2.matchTemplate(img, pattern[1], cv2.TM_CCOEFF_NORMED)
+            loc = np.where(r >= np.array(threshold))
+
+            for pt in zip(*loc[::-1]):
+                print("found '%s'" % pattern[0])
+
+                inputs = match_inputs(pattern[0])
+                if inputs is None:
+                    continue
+
+                for i in inputs:
+                    if type(i) == float:
+                        time.sleep(i)
+                    else:
+                        win32api.PostMessage(hwndMain, win32con.WM_KEYDOWN, i, 0)
+                        time.sleep(0.05)
+                        win32api.PostMessage(hwndMain, win32con.WM_KEYUP, i, 0)
+
+                break
+
+
+if __name__ == "__main__":
+    main()
